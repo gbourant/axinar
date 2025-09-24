b9ba17a80d3a6cb98e52e02d978025d4783e6465
gbourant <root@gbourant.com>
2025-09-24 19:19:37 +0300

    convert Instant to EpochMillis

---
+++ src/main/java/com/axinar/config/JsonbCustomizer.java
@@ -0,0 +1,13 @@
+package com.axinar.config;
+
+import io.quarkus.jsonb.JsonbConfigCustomizer;
+import jakarta.inject.Singleton;
+import jakarta.json.bind.JsonbConfig;
+
+@Singleton
+public class JsonbCustomizer implements JsonbConfigCustomizer {
+
+    public void customize(JsonbConfig config) {
+        config.withAdapters(new InstantEpochMillisAdapter());
+    }
+}
